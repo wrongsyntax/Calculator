9b163d91ff3bd5e22fe5c91ae0da0f0e93e09905
Uzair <uzair0213@icloud.com>
2021-06-09 20:18:15 -0600

    Initial commit, Xcode project created

---
+++ calcula/calcula/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calcula
+//
+//  Created by Uzair Tariq on 2021-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calcula/calcula/calculaApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculaApp.swift
+//  calcula
+//
+//  Created by Uzair Tariq on 2021-06-09.
+//
+
+import SwiftUI
+
+@main
+struct calculaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
